e2c9549b6af9251aba5fcbc6f17a47acf9ad92ee
Sierra-Springs <nathanael-1999@hotmail.fr>
2022-11-29 10:50:05 +0100

    Code de test des modèles et Test de SVM

--- src/classification.py
+++ src/classification.py
@@ -2,48 +2,15 @@
 # Découper les données en apprentissage = {train, dev}, validation, test
 # Ré-équilibrer les classes (pondération des erreurs ou génération de données)
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from sklearn import svm
-from sklearn.dummy import DummyClassifier
-from sklearn.metrics import confusion_matrix
+from pprint import pprint
 
-from Data.DataProvider import DataProvider
-from Utils.pathsDefinition import *
-from Utils.ColorPrinter import *
-from Utils.names import *
-
-import pandas as pd
+from Models.SVM import *
 
 if __name__ == '__main__':
-    print(table1Path)
-    dataProvider = DataProvider()
-    data = dataProvider.get_data_prepare(.60, .20, .20)
+    svmBase = SVMBaseNoWeights()
+    metrics = svmBase.evaluate()
 
-    for key in data.keys():
-        blueprint(key)
-        print(data[key]["X"])
-        print()
-
-    clfs = [svm.SVC(gamma="scale"), DummyClassifier()]
-    clfs_names = ["svm", "dummy"]
-
-    for clf, clf_name in zip(clfs, clfs_names):
-
-        clf.fit(data[TRAIN]["X"], data[TRAIN]["Y"])
-
-        print('Accuracy of ' + clf_name + ' classifier on training set: {:.2f}'
-              .format(clf.score(data[TRAIN]["X"], data[TRAIN]["Y"])))
-
-        print('Accuracy of ' + clf_name + ' classifier on valid set: {:.2f}'
-              .format(clf.score(data[VALID]["X"], data[VALID]["Y"])))
-
-        print('Accuracy of ' + clf_name + ' classifier on test set: {:.2f}'
-              .format(clf.score(data[TEST]["X"], data[TEST]["Y"])))
-
-        y_pred = clf.predict(data[VALID]["X"])
-        print(confusion_matrix(data[VALID]["Y"], y_pred))
-
-        y_pred = clf.predict(data[TEST]["X"])
-        print(confusion_matrix(data[TEST]["Y"], y_pred))
-
-        blueprint("_" * 30)
+    pprint(metrics)
